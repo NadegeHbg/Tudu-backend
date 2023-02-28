01d739bb32924700374714076ea4dbba17d74ca3
obaida-Becode <zakaria.obz@hotmail.com>
2023-02-28 14:38:13 +0100

    update register schema

--- controllers/loginCRUD/userRegister.mjs
+++ controllers/loginCRUD/userRegister.mjs
@@ -6,28 +6,40 @@ import passportConfig from "../../config/passport.mjs";
 // import ensureAuthenticated from "../../config/auth.mjs";
 
 // Register Form
-const userRegister = asyncHandler(async (req, res) => {
-  const { email, password } = req.body;
-  
+const userRegister = asyncHandler(async (req, res,next) => {
+
+  const { email, password,conformation,username,firstname,secondname } = req.body;
+  console.log(req.body, "register with frontend");
+
   let errors = [];
 
-  if (!email || !password) {
+  if (!email || !password || !conformation || !username ) {
     errors.push({ msg: "Please fill in all fields" });
   }
-  
-  if (password.length < 6) {
-    errors.push({ msg: "Password must be at least 6 characters" });
-  }
 
-  if (errors.length > 0 ) {
-    res.render('register', {
+   //check if match
+if(password !== conformation) {
+  errors.push({msg : "passwords dont match"});
+}
+
+  // if (password.length < 6) {
+  //   errors.push({ msg: "Password must be at least 6 characters" });
+  // }
+ 
+
+  if (errors.length > 0) {
+    res.send("register", {
       errors: errors,
       email: email,
-      password: password
+      password: password,
+      conformation:conformation,
+      username:username,
+      firstname:firstname,
+      secondname:secondname
     });
   } else {
     const userExists = await User.findOne({ email: email });
-    
+
     try {
       if (userExists) {
         errors.push({ msg: "Email already registered" });
@@ -37,23 +49,22 @@ const userRegister = asyncHandler(async (req, res) => {
         const newUser = new User({
           email: email,
           password: hashedPassword,
+          conformation:hashedPassword,
+          username: username,
+          firstname: firstname,
+          secondname: secondname,
         });
         await newUser.save();
+        // console.log(newUser);
       }
     } catch (error) {
       console.error(error);
-      
     }
 
-
-      return res.send("user registered")
-
-      
-    }
+    return res.send("user registered");
   }
-);
+});
 
 passportConfig(passport);
 
 export default userRegister;
-
