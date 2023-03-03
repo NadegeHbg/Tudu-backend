7dcee02b70b003f9f305e71d71d9b3f1187e474e
obaida-Becode <zakaria.obz@hotmail.com>
2023-03-03 12:25:37 +0100

    push all

---
+++ opt/render/project/src/server/index.mjs
@@ -0,0 +1,74 @@
+
+import express from "express";
+import morgan from "morgan";
+import dbConnection from "./config/database.mjs";
+import dotenv from "dotenv";
+import router from "./routes/tuduRoute.mjs";
+import session from "express-session";
+
+import bodyParser from "body-parser";
+// import session from "express-session";
+// import flash from "connect-flash";
+import passport from "passport";
+import ensureAuthenticated from "./config/auth.mjs";
+
+
+dotenv.config();
+
+// connect with database
+dbConnection();
+
+// app express
+const app = express();
+// BodyParser
+// Use body-parser to parse JSON data sent via HTTP POST
+app.use(bodyParser.json());
+app.use(express.urlencoded({extended : true}))
+
+// morgan midlleweare
+app.use(express.json()); // to make parcing for json
+if (process.env.Node_env === "development") {
+  app.use(morgan("dev"));
+  console.log(`mode:${process.env.Node_env}`);
+};
+//Mount Routes
+app.use("/api/tudu/", router);
+
+app.get("/", (req, res) => {
+  res.send("Welcome in TuDu", router);
+});
+
+
+
+app.use(session({
+  secret: 'your-secret-key',
+  resave: false,
+  saveUninitialized: true
+}));
+
+
+app.post("/login", (req, res, next) => {
+  passport.authenticate("local", {
+    
+  })
+  (req, res, next);
+});
+
+
+app.get("/dashboard", ensureAuthenticated, (req, res) => {
+  console.log(req);
+  res.render("dashboard", { user: req.user });
+});
+
+//logout
+app.post("/logout", (req, res) => {
+  req.logout();
+
+  res.send("/user/login");
+});
+
+
+const port = process.env.port || 5000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
